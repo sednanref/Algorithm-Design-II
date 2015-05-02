3b3ee239525bf763323ab6a11512adcabeb2aaec
sednanref <sednanref2004@gmail.com>
2015-05-01 21:24:05 -0430

    Getting the objective function, it has some bugs yet.

--- test/test.cpp
+++ test/test.cpp
@@ -11,16 +11,26 @@ vector< vector <int> > flow_matrix;
 //Matrix with the distance of the problem.
 vector< vector <int> > distance_matrix;
 
+
+
 /*
 	Function that returns a random solution.x
 */
 void random_solution(){
 	
-	for(int i = 0; i < 10; i++) solution.push_back(i);
+	for(int i = 0; i < n; i++) solution.push_back(i);
 	//randomize the seed with the actual time.
 	srand(time(0)); 
 	//randomize the solution.
 	random_shuffle(solution.begin(), solution.end());	
+}
+
+int objective_function(){
+	int cost = 0;
+	for (int i = 0; i < n; i++){
+		cost = cost + flow_matrix[i][solution[i]] * distance_matrix[i][solution[i]];
+	}
+	return cost;
 }
 
 int main(){
@@ -45,6 +55,8 @@ int main(){
 		}
 	}
 
+	//OUTPUT
+	cout<<n<<endl<<endl;
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
 			cout<<flow_matrix[i][j]<<" ";
@@ -60,10 +72,29 @@ int main(){
 	}
 
 	//get a solution
-	random_solution();
+//	random_solution();
+	solution.clear();
+	solution.push_back(1);
+	solution.push_back(13);
+	solution.push_back(9);
+	solution.push_back(15);
+	solution.push_back(4);
+	solution.push_back(2);
+	solution.push_back(6);
+	solution.push_back(7);
+	solution.push_back(3);
+	solution.push_back(5);
+	solution.push_back(11);
+	solution.push_back(10);
+	solution.push_back(14);
+	solution.push_back(12);
+	solution.push_back(18);
+	solution.push_back(0);
 
 	//output
-	for(int i = 0; i < 10; i++){
-		cout<<solution[i]<<endl;
+	for(int i = 0; i < n; i++){
+		cout<<solution[i]<<" ";
 	}
+
+	cout<<"    "<<objective_function()<<endl;
 }

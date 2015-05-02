bde3413dd68cbb49c60424a806fb94f84e08dbe3
sednanref <sednanref2004@gmail.com>
2015-05-01 19:42:22 -0430

    Reading the input, done.

--- test/test.cpp
+++ test/test.cpp
@@ -2,27 +2,68 @@
 //#include "qap_utils.h"
 using namespace std;
 
+//Size of the problem.
+int n;
+//Vector with a Solution for the problem.
 vector<int> solution;
+//Matrix with the flow of the problem.
+vector< vector <int> > flow_matrix;
+//Matrix with the distance of the problem.
+vector< vector <int> > distance_matrix;
 
-
-vector<int>  random_solution(){
+/*
+	Function that returns a random solution.x
+*/
+void random_solution(){
 	
 	for(int i = 0; i < 10; i++) solution.push_back(i);
-
 	//randomize the seed with the actual time.
 	srand(time(0)); 
 	//randomize the solution.
-	random_shuffle(solution.begin(), solution.end());
-
-	return solution;
-	
+	random_shuffle(solution.begin(), solution.end());	
 }
 
 int main(){
+	//read the size of the problem.
+	cin>>n;
 
+	//read the flow matrix
+	flow_matrix.resize(n);
+	for(int i = 0; i < n; i++){
+		flow_matrix[i].resize(n);
+		for(int j = 0; j < n; j++){
+			cin>>flow_matrix[i][j];
+		}
+	}
 
+	//read the distance matrix
+	distance_matrix.resize(n);
+	for(int i = 0; i < n; i++){
+		distance_matrix[i].resize(n);
+		for(int j = 0; j < n; j++){
+			cin>>distance_matrix[i][j];
+		}
+	}
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cout<<flow_matrix[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cout<<distance_matrix[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+
+	//get a solution
+	random_solution();
+
+	//output
 	for(int i = 0; i < 10; i++){
 		cout<<solution[i]<<endl;
 	}
-	//cout<<rand()<<endl;
 }

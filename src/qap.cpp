32dbd6e0493a56f8cc44dfbd89d2bdc1f68ee285
sednanref <sednanref2004@gmail.com>
2015-05-02 22:08:49 -0430

    The code is now refactored.

--- src/qap.cpp
+++ src/qap.cpp
@@ -13,19 +13,43 @@ vector< vector <int> > distance_matrix;
 //Vector with the neighbourhood and their evaluation.
 vector< pair < vector <int> , int > > neighbourhood;
 
+/*
+	Procedure that reads the input.
+*/
+void read_input(){
+	//read the size of the problem.
+	cin>>n;
+	
+	//read the flow matrix
+	flow_matrix.resize(n);
+	for(int i = 0; i < n; i++){
+		flow_matrix[i].resize(n);
+		for(int j = 0; j < n; j++){
+			cin>>flow_matrix[i][j];
+		}
+	}
+
+	//read the distance matrix
+	distance_matrix.resize(n);
+	for(int i = 0; i < n; i++){
+		distance_matrix[i].resize(n);
+		for(int j = 0; j < n; j++){
+			cin>>distance_matrix[i][j];
+		}
+	}
+}
 
 /*
 	Function that returns a random solution.x
 */
 void random_solution(){
-	
+	//Assign to the i-th facility the i-th location.
 	for(int i = 0; i < n; i++) solution.push_back(i);
 	//randomize the seed with the actual time.
 	srand(time(0)); 
 	//randomize the solution.
 	random_shuffle(solution.begin(), solution.end());
 }
-
 
 /*
 	Evaluation Function of the problem.
@@ -48,15 +72,16 @@ bool pair_is_less(pair <int, int> a, pair <int, int> b){
 	return a.second < b.second;
 }
 
-
 /*
 	Procedure that generates a Greedy Solution to the problem.
 */
 void greedy_solution(){
+	//Vector with the facilities and their total flow.
 	vector< pair <int, int> > facilities_total_flow;
+	//Vector with the locations and their total distance.
 	vector< pair <int, int> > location_total_distances;
 
-	/*Getting the total flow of each facility*/
+	//Getting the total flow of each facility
 	for(int i = 0; i < n; i++){
 		int total_flow = 0;
 		for(int j = 0; j < n; j++){
@@ -64,10 +89,11 @@ void greedy_solution(){
 		}
 		facilities_total_flow.push_back(make_pair(i,total_flow));
 	}
+
 	//Sort the facilities by their total flow.
 	sort(facilities_total_flow.begin(), facilities_total_flow.end(), pair_is_less);
 
-	/*Getting the total distance of each location*/
+	//Getting the total distance of each location
 	for(int i = 0; i < n; i++){
 		int total_distance = 0;
 		for(int j = 0; j < n; j++){
@@ -75,6 +101,7 @@ void greedy_solution(){
 		}
 		location_total_distances.push_back(make_pair(i,total_distance));
 	}
+
 	//Sort the locations by their total distance.
 	sort(location_total_distances.begin(), location_total_distances.end(), pair_is_less);
 
@@ -83,9 +110,13 @@ void greedy_solution(){
 	for(int i = 0; i < n; i++){
 		solution[facilities_total_flow[n-i].first] = location_total_distances[i].first;
 	}
-
 }
 
+/*
+	Procedure that generates the neighbourhood of the actual solution,
+	it also evaluate each neighbour solution and save it in the 
+	neighbourhood vector.
+*/
 void generate_evaluated_neighbourhood(){
 	//Auxiliar neighbour (for rotation)
 	vector <int> aux_solution;
@@ -118,123 +149,51 @@ void generate_evaluated_neighbourhood(){
 		//Add the rotated solution to the neighbourhood.
 		neighbourhood.push_back(make_pair(aux_solution,evaluation_function(aux_solution)));
 	}
-	//Print the neighbourhood, just to test.
-	/*
-	for(int i = 0; i < neighbourhood.size(); i++){
-		vector <int> aux = neighbourhood[i].first;
-		for(int j = 0; j < n; j++){
-			cout<<aux[j]<<" ";
-		}
-		cout<<" -> "<<neighbourhood[i].second<<endl;
-	}
-	*/
 }
 
+/*
+	Procedure that 
+*/
 void basic_local_search(){
+	//best value in the local search, 
+	//initialized with the value of the initial solution
 	int best_value = evaluation_function(solution);
-	vector < int > best_neighbourhood_solution = solution;
-	int q = 5;
-	while(q--){
+	//Best solution of the neighbourhood of the actual solution,
+	//initialized with the initial solution.
+	vector <int> best_neighbourhood_solution = solution;
+	//Number of iterations as the stop condition.
+	int iterations = 20;
+
+	while(iterations--){
+		//generate a neighbourhood of the actual solution.
 		generate_evaluated_neighbourhood();
 		bool solution_is_changed = false;
+		//Loof for the best solution in the neighbourhood.
 		for(int i = 0; i < neighbourhood.size(); i++){
+			//
 			if(neighbourhood[i].second < best_value){
 				solution_is_changed = true;
 				best_neighbourhood_solution = neighbourhood[i].first;
 				best_value = neighbourhood[i].second;
 			}
 		}
-		
+		//If it doesn't find a better solution, quit the local search.
 		if(!solution_is_changed) break;
+		//Update the actual solution.
 		solution = best_neighbourhood_solution;
-		for(int i = 0; i < n; i++){
-			cout<<best_neighbourhood_solution[i]<<" ";
-		}
-		cout<<" -> "<<best_value<<endl;
 	}
 }
 
 int main(){
-	//read the size of the problem.
-	cin>>n;
-	
 
-
-	//read the flow matrix
-	flow_matrix.resize(n);
-	for(int i = 0; i < n; i++){
-		flow_matrix[i].resize(n);
-		for(int j = 0; j < n; j++){
-			cin>>flow_matrix[i][j];
-		}
-	}
-
-	//read the distance matrix
-	distance_matrix.resize(n);
-	for(int i = 0; i < n; i++){
-		distance_matrix[i].resize(n);
-		for(int j = 0; j < n; j++){
-			cin>>distance_matrix[i][j];
-		}
-	}
-
-	//Printin Matrixes
-/*	cout<<n<<endl<<endl;
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			cout<<flow_matrix[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-	cout<<endl;
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			cout<<distance_matrix[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-*/
+	read_input();
 	//get a solution
-	//random_solution();
-	greedy_solution();
-	
-//	for chr12a.dat
-/*	solution.clear();
-	solution.push_back(6);
-	solution.push_back(4);
-	solution.push_back(11);
-	solution.push_back(1);
-	solution.push_back(0);
-	solution.push_back(2);
-	solution.push_back(8);
-	solution.push_back(10);
-	solution.push_back(9);
-	solution.push_back(5);
-	solution.push_back(7);
-	solution.push_back(3);
-*/
-//	for esc16a.
-	/*
-	solution.push_back(1);
-	solution.push_back(13);
-	solution.push_back(9);
-	solution.push_back(15);
-	solution.push_back(4);
-	solution.push_back(2);
-	solution.push_back(6);
-	solution.push_back(7);
-	solution.push_back(3);
-	solution.push_back(5);
-	solution.push_back(11);
-	solution.push_back(10);	
-	solution.push_back(14);
-	solution.push_back(12);
-	solution.push_back(8);
-	solution.push_back(0);	
-*/
+	random_solution();
+	//greedy_solution();
 	
 	cout<<"First Solution: "<<evaluation_function(solution)<<endl;
 	
+	//Do the local search.
 	basic_local_search();
 
 	cout<<"Local search Solution: "<<evaluation_function(solution)<<endl;

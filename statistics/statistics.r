01940d9633a8a0d6d0da02ad2ac6dd669bf1a9cf
sednanref <sednanref2004@gmail.com>
2015-05-30 20:23:11 -0430

    R script to load the results.

---
+++ statistics/statistics.r
@@ -0,0 +1,77 @@
+rm(list=ls()) #Remove the data.
+#Load the data.
+#nug12
+ils_nug12_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug12.dat.txt")
+ils_nug12 = ils_nug12_dat[,1]
+ts_nug12_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug12.dat.txt")
+ts_nug12 = ts_nug12_dat[,1]
+#nug14
+ils_nug14_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug14.dat.txt")
+ils_nug14 = ils_nug14_dat[,1]
+ts_nug14_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug14.dat.txt")
+ts_nug14 = ts_nug14_dat[,1]
+#nug15
+ils_nug15_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug15.dat.txt")
+ils_nug15 = ils_nug15_dat[,1]
+ts_nug15_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug15.dat.txt")
+ts_nug15 = ts_nug15_dat[,1]
+#nug16a
+ils_nug16a_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug16a.dat.txt")
+ils_nug16a = ils_nug16a_dat[,1]
+ts_nug16a_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug16a.dat.txt")
+ts_nug16a = ts_nug16a_dat[,1]
+#nug16b
+ils_nug16b_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug16b.dat.txt")
+ils_nug16b = ils_nug16b_dat[,1]
+ts_nug16b_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug16b.dat.txt")
+ts_nug16b = ts_nug16b_dat[,1]
+#nug17
+ils_nug17_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug17.dat.txt")
+ils_nug17 = ils_nug17_dat[,1]
+ts_nug17_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug17.dat.txt")
+ts_nug17 = ts_nug17_dat[,1]
+#nug18
+ils_nug18_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug18.dat.txt")
+ils_nug18 = ils_nug18_dat[,1]
+ts_nug18_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug18.dat.txt")
+ts_nug18 = ts_nug18_dat[,1]
+#nug20
+ils_nug20_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug20.dat.txt")
+ils_nug20 = ils_nug20_dat[,1]
+ts_nug20_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug20.dat.txt")
+ts_nug20 = ts_nug20_dat[,1]
+#nug21
+ils_nug21_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug21.dat.txt")
+ils_nug21 = ils_nug21_dat[,1]
+ts_nug21_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug21.dat.txt")
+ts_nug21 = ts_nug21_dat[,1]
+#nug22
+ils_nug22_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug22.dat.txt")
+ils_nug22 = ils_nug22_dat[,1]
+ts_nug22_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug22.dat.txt")
+ts_nug22 = ts_nug22_dat[,1]
+#nug24
+ils_nug24_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug24.dat.txt")
+ils_nug24 = ils_nug24_dat[,1]
+ts_nug24_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug24.dat.txt")
+ts_nug24 = ts_nug24_dat[,1]
+#nug25
+ils_nug25_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug25.dat.txt")
+ils_nug25 = ils_nug25_dat[,1]
+ts_nug25_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug25.dat.txt")
+ts_nug25 = ts_nug25_dat[,1]
+#nug27
+ils_nug27_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug27.dat.txt")
+ils_nug27 = ils_nug27_dat[,1]
+ts_nug27_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug27.dat.txt")
+ts_nug27 = ts_nug27_dat[,1]
+#nug28
+ils_nug28_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug28.dat.txt")
+ils_nug28 = ils_nug28_dat[,1]
+ts_nug28_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug28.dat.txt")
+ts_nug28 = ts_nug28_dat[,1]
+#nug30
+ils_nug30_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ils/nug30.dat.txt")
+ils_nug30 = ils_nug30_dat[,1]
+ts_nug30_dat = read.table("/home/lfernandes/Algorithm-Design-II/results/ts/nug30.dat.txt")
+ts_nug30 = ts_nug30_dat[,1]
